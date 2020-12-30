46788893c75ecefeaba573a753a0f2a8d2b3dca3
Ruben Gargiulo <58166800+raeubaen@users.noreply.github.com>
2020-12-30 10:14:16 +0100

    Update bot_config.py

--- Bot/bot_config.py
+++ Bot/bot_config.py
@@ -1,5 +1,5 @@
 bot_config = {
     "admin_id": 788342587,
     "max_team_size": 11,
-    "token": "817553069:AAGXUHbp24kULpINKP1OrAG6luaUJp3CEq0",
+    "token": "817553069:AAGXpHbp29mULpINKP1OrAG6luaUJl3CEq0",
 }
